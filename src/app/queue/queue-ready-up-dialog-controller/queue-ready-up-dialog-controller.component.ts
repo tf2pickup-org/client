e02ff895b1753635f74d4b5706be9c561c58d56f
Michał Garapich <michal@garapich.pl>
2019-07-25 13:22:09 +0200

    bring back old ready up sound

--- src/app/queue/queue-ready-up-dialog-controller/queue-ready-up-dialog-controller.component.ts
+++ src/app/queue/queue-ready-up-dialog-controller/queue-ready-up-dialog-controller.component.ts
@@ -17,7 +17,7 @@ export class QueueReadyUpDialogControllerComponent implements OnInit, OnDestroy 
 
   private destroyed = new Subject<void>();
   private queueReadyUpDialogRef: BsModalRef;
-  private audio = new Audio('/assets/sounds/ready_up_wonszu.wav');
+  private audio = new Audio('/assets/sounds/ready_up.wav');
 
   constructor(
     private store: Store<AppState>,
