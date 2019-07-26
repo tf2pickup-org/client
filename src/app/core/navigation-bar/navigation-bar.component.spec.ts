c29d62a5d3baeddf5f3447e5e79a944bce65cd9d
Michał Garapich <michal@garapich.pl>
2019-07-26 02:28:03 +0200

    feat: add navbar logo

--- src/app/core/navigation-bar/navigation-bar.component.spec.ts
+++ src/app/core/navigation-bar/navigation-bar.component.spec.ts
@@ -40,15 +40,20 @@ describe('NavigationBarComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should render the logo', () => {
+    const el = fixture.debugElement.query(By.css('a.navbar-brand>img'));
+    expect(el).toBeTruthy();
+  });
+
   it('app-steam-login-button should be rendered if user is not authenticated', () => {
     const el = fixture.debugElement.query(By.css('app-steam-login-button'));
     expect(el).toBeTruthy();
   });
 
-  it('should render profile name in h2 tag', () => {
+  it('should render profile name in a h4 tag', () => {
     store.setState({ profile: { profile: { name: 'FAKE_NAME' } } });
     fixture.detectChanges();
-    const el = fixture.debugElement.query(By.css('h2'));
+    const el = fixture.debugElement.query(By.css('h4'));
     expect(el).toBeTruthy();
     expect((el.nativeElement as HTMLElement).innerText).toBe('FAKE_NAME');
   });
