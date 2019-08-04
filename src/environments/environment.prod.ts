d783cddc908c03ad50d9c5aca1adf23f455c06e6
Michał Garapich <michal@garapich.pl>
2019-08-04 14:09:28 +0200

    update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,17 @@
 import { version } from 'package.json';
 
+const discordInvitation = 'https://discord.gg/YsAAAz';
+
 export const environment = {
   production: true,
   apiUrl: 'https://api.tf2pickup.pl',
   wsUrl: 'https://api.tf2pickup.pl',
   version,
+  headerLinks: [
+    { name: 'discord', icon: '/assets/discord.svg', target: discordInvitation, tooltip: 'Join us on discord!' },
+  ],
   footerLinks: [
     { name: 'github', target: 'https://github.com/tf2pickup-pl' },
-    { name: 'discord', target: 'https://discord.gg/YsAAAz' },
+    { name: 'discord', target: discordInvitation },
   ],
 };
