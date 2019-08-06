7bbd6fac6f75c400160df11d432e1724b8962ec9
Michał Garapich <michal@garapich.pl>
2019-08-06 12:33:14 +0200

    feat: sort players by name (#22)

--- src/app/players/players.adapter.ts
+++ src/app/players/players.adapter.ts
@@ -1,4 +1,10 @@
 import { EntityAdapter, createEntityAdapter } from '@ngrx/entity';
 import { Player } from './models/player';
 
-export const adapter: EntityAdapter<Player> = createEntityAdapter<Player>();
+function sortByName(a: Player, b: Player): number {
+  return a.name.localeCompare(b.name);
+}
+
+export const adapter: EntityAdapter<Player> = createEntityAdapter<Player>({
+  sortComparer: sortByName,
+});
