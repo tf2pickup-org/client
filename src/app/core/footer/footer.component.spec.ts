1d63e287121110cde4349efb126957954ef9014a
Michał Garapich <michal@garapich.pl>
2019-08-06 12:32:41 +0200

    feat: logout link (#21)
    * add logout link at footer
    * add tokenStore.removeAllTokens()
    * logout implementation
    * move sign in through steam button to assets

--- src/app/core/footer/footer.component.spec.ts
+++ src/app/core/footer/footer.component.spec.ts
@@ -1,5 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FooterComponent } from './footer.component';
+import { TokenStoreService } from '@app/auth/token-store.service';
+import { AuthService } from '@app/auth/auth.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ChangeDetectionStrategy } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
+class TokenStoreServiceStub {
+  removeAllTokens() { }
+}
+
+class AuthServiceStub {
+  authenticated: false;
+}
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
@@ -7,8 +20,17 @@ describe('FooterComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FooterComponent ]
+      declarations: [ FooterComponent ],
+      imports: [
+        RouterTestingModule,
+      ],
+      providers: [
+        { provide: TokenStoreService, useClass: TokenStoreServiceStub },
+        { provide: AuthService, useClass: AuthServiceStub },
+      ]
     })
+    // https://github.com/angular/angular/issues/12313
+    .overrideComponent(FooterComponent, { set: { changeDetection: ChangeDetectionStrategy.Default } })
     .compileComponents();
   }));
 
@@ -20,5 +42,26 @@ describe('FooterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render logout button only if the user is authenticated', () => {
+    component.links = [];
+    fixture.detectChanges();
+    expect(fixture.debugElement.query(By.css('a'))).toBeNull();
+
+    component.isAuthenticated = true;
+    fixture.detectChanges();
+    const el = fixture.debugElement.query(By.css('a')).nativeElement as HTMLAnchorElement;
+    expect(el).toBeTruthy();
+    expect(el.innerText).toBe('logout');
+  });
+
+  describe('logout()', () => {
+    xit('should remove all tokens from the local storage', () => {
+      // todo handle window.location.reload
+      const spy = spyOn(TestBed.get(TokenStoreService), 'removeAllTokens');
+      component.logout();
+      expect(spy).toHaveBeenCalled();
+    });
   });
 });
