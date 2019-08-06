a121cd017db105ba2c6bfb0cdacbddcaff398bc4
Michał <michal@garapich.pl>
2019-08-06 12:23:42 +0200

    update tests

--- src/app/core/footer/footer.component.spec.ts
+++ src/app/core/footer/footer.component.spec.ts
@@ -3,6 +3,8 @@ import { FooterComponent } from './footer.component';
 import { TokenStoreService } from '@app/auth/token-store.service';
 import { AuthService } from '@app/auth/auth.service';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ChangeDetectionStrategy } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 class TokenStoreServiceStub {
   removeAllTokens() { }
@@ -27,6 +29,8 @@ describe('FooterComponent', () => {
         { provide: AuthService, useClass: AuthServiceStub },
       ]
     })
+    // https://github.com/angular/angular/issues/12313
+    .overrideComponent(FooterComponent, { set: { changeDetection: ChangeDetectionStrategy.Default } })
     .compileComponents();
   }));
 
@@ -38,6 +42,18 @@ describe('FooterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render logout button only if the user is authenticated', () => {
+    component.links = [];
+    fixture.detectChanges();
+    expect(fixture.debugElement.query(By.css('a'))).toBeNull();
+
+    component.isAuthenticated = true;
+    fixture.detectChanges();
+    const el = fixture.debugElement.query(By.css('a')).nativeElement as HTMLAnchorElement;
+    expect(el).toBeTruthy();
+    expect(el.innerText).toBe('logout');
   });
 
   describe('logout()', () => {
