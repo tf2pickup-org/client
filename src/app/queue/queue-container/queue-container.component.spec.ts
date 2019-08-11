adaef04ce0ee24dd1ba350938ddb6b74d36e7e6d
Michał Garapich <michal@garapich.pl>
2019-08-11 03:20:11 +0200

    QueueContainerComponent: test update

--- src/app/queue/queue-container/queue-container.component.spec.ts
+++ src/app/queue/queue-container/queue-container.component.spec.ts
@@ -1,15 +1,18 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { QueueContainerComponent } from './queue-container.component';
 import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { Store } from '@ngrx/store';
 import { RouterTestingModule } from '@angular/router/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { activeGame } from '@app/games/games.selectors';
+import { queueRequiredPlayerCount, queueCurrentPlayerCount } from '../queue.selectors';
+import { Title } from '@angular/platform-browser';
 
 describe('QueueContainerComponent', () => {
   let component: QueueContainerComponent;
   let fixture: ComponentFixture<QueueContainerComponent>;
   let store: MockStore<any>;
+  let setTitleSpy: jasmine.Spy;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -18,7 +21,13 @@ describe('QueueContainerComponent', () => {
         RouterTestingModule,
       ],
       providers: [
-        provideMockStore(),
+        provideMockStore({
+          selectors: [
+            { selector: activeGame, value: null },
+            { selector: queueRequiredPlayerCount, value: 12 },
+            { selector: queueCurrentPlayerCount, value: 5 },
+          ]
+        }),
       ],
       schemas: [ NO_ERRORS_SCHEMA ],
     })
@@ -27,7 +36,7 @@ describe('QueueContainerComponent', () => {
 
   beforeEach(() => {
     store = TestBed.get(Store);
-    store.overrideSelector(activeGame, null);
+    setTitleSpy = spyOn(TestBed.get(Title), 'setTitle');
 
     fixture = TestBed.createComponent(QueueContainerComponent);
     component = fixture.componentInstance;
@@ -39,4 +48,13 @@ describe('QueueContainerComponent', () => {
   });
 
   it('should show active game link');
+
+  describe('ngOnInit()', () => {
+    it('should set the page title', fakeAsync(() => {
+      component.ngOnInit();
+      tick(200);
+      expect(setTitleSpy).toHaveBeenCalled();
+      expect(setTitleSpy.calls.mostRecent().args[0]).toMatch(/^\[5\/12\]/);
+    }));
+  });
 });
