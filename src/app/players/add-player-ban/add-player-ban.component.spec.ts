d78bc8da76516832812ca3d4ecaad9fe6e75c927
Michał Garapich <michal@garapich.pl>
2019-08-12 18:43:22 +0200

    add AddPlayerBansComponent tests

--- src/app/players/add-player-ban/add-player-ban.component.spec.ts
+++ src/app/players/add-player-ban/add-player-ban.component.spec.ts
@@ -3,10 +3,15 @@ import { AddPlayerBanComponent } from './add-player-ban.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { provideMockStore } from '@ngrx/store/testing';
-import { Subject } from 'rxjs';
+import { Subject, of } from 'rxjs';
 import { Action } from '@ngrx/store';
 import { Actions } from '@ngrx/effects';
+import { Location } from '@angular/common';
+import { Router, convertToParamMap, ActivatedRoute } from '@angular/router';
+import { playerBanAdded } from '../actions';
+import { PlayerBan } from '../models/player-ban';
 
+const paramMap = of(convertToParamMap({ id: 'FAKE_ID' }));
 const actions = new Subject<Action>();
 
 describe('AddPlayerBanComponent', () => {
@@ -37,6 +42,7 @@ describe('AddPlayerBanComponent', () => {
       providers: [
         provideMockStore({ initialState }),
         { provide: Actions, useValue: actions },
+        { provide: ActivatedRoute, useValue: { paramMap } },
       ]
     })
     .compileComponents();
@@ -50,5 +56,19 @@ describe('AddPlayerBanComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should redirect to player ban list when adding a ban is done', () => {
+    const spy = spyOn(TestBed.get(Router), 'navigate');
+    actions.next(playerBanAdded({ playerBan: { player: 'FAKE_ID' } } as { playerBan: PlayerBan }));
+    expect(spy).toHaveBeenCalledWith(['/player', 'FAKE_ID', 'bans']);
+  });
+
+  describe('#cancel()', () => {
+    it('should call location.back()', () => {
+      const spy = spyOn(TestBed.get(Location), 'back');
+      component.cancel();
+      expect(spy).toHaveBeenCalled();
+    });
   });
 });
