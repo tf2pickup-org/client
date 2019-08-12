81761ff629b093cb6cc6d622b8b0cc283b58dc58
Michał Garapich <michal@garapich.pl>
2019-08-12 18:22:35 +0200

    update PlayerBanItemComponent tests

--- src/app/players/player-ban-item/player-ban-item.component.spec.ts
+++ src/app/players/player-ban-item/player-ban-item.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { PlayerBanItemComponent } from './player-ban-item.component';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { NO_ERRORS_SCHEMA, ChangeDetectionStrategy } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('PlayerBanItemComponent', () => {
   let component: PlayerBanItemComponent;
@@ -11,6 +12,8 @@ describe('PlayerBanItemComponent', () => {
       declarations: [ PlayerBanItemComponent ],
       schemas: [ NO_ERRORS_SCHEMA ],
     })
+    // https://github.com/angular/angular/issues/12313
+    .overrideComponent(PlayerBanItemComponent, { set: { changeDetection: ChangeDetectionStrategy.Default } })
     .compileComponents();
   }));
 
@@ -23,4 +26,59 @@ describe('PlayerBanItemComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should determine whether the ban has expired', () => {
+    const start = new Date();
+    let end = new Date();
+    end.setHours(end.getHours() + 1);
+
+    component.playerBan = {
+      player: 'FAKE_PLAYER_ID',
+      start,
+      end,
+      reason: 'FAKE_PLAYER_BAN_REASON',
+      admin: 'FAKE_ADMIN_ID',
+      id: 'FAKE_PLAYER_BAN_ID'
+    };
+    expect(component.expired).toBe(false);
+
+    end = new Date();
+    end.setSeconds(end.getSeconds() - 1);
+    component.playerBan = {
+      player: 'FAKE_PLAYER_ID',
+      start,
+      end,
+      reason: 'FAKE_PLAYER_BAN_REASON',
+      admin: 'FAKE_ADMIN_ID',
+      id: 'FAKE_PLAYER_BAN_ID'
+    };
+    expect(component.expired).toBe(true);
+  });
+
+  describe('with player ban', () => {
+    beforeEach(() => {
+      const start = new Date();
+      start.setHours(start.getHours() - 1);
+
+      const end = new Date();
+      end.setHours(end.getHours() + 1);
+
+      component.playerBan = {
+        player: 'FAKE_PLAYER_ID',
+        start,
+        end,
+        reason: 'FAKE_PLAYER_BAN_REASON',
+        admin: 'FAKE_ADMIN_ID',
+        id: 'FAKE_PLAYER_BAN_ID'
+      };
+      fixture.detectChanges();
+    });
+
+    it('should emit revoke event', async(() => {
+      component.revoke.subscribe(ban => expect(ban).toEqual(component.playerBan));
+      const btn = fixture.debugElement.query(By.css('button[type=button]')).nativeElement as HTMLButtonElement;
+      btn.click();
+    }));
+  });
+
 });
