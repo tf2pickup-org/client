32570aff99bfce2147ab3d24c70a46689461ae3e
Michał Garapich <michal@garapich.pl>
2019-08-14 04:01:41 +0200

    revamp GameDetailsComponent
    * better players' obervables handling
    * fetch game skills and render them
    * unsubscribe on destroy

--- src/app/games/game-details/game-details.component.ts
+++ src/app/games/game-details/game-details.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit, ChangeDetectionStrategy, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ViewChild, ElementRef, OnDestroy } from '@angular/core';
 import { Store, select } from '@ngrx/store';
 import { AppState } from '@app/app.state';
-import { Observable, zip } from 'rxjs';
+import { Observable, Subject, ReplaySubject, combineLatest } from 'rxjs';
 import { Game } from '../models/game';
 import { ActivatedRoute } from '@angular/router';
-import { switchMap, map, tap, filter, first, pairwise } from 'rxjs/operators';
+import { switchMap, map, tap, filter, first, pairwise, withLatestFrom, shareReplay, takeUntil } from 'rxjs/operators';
 import { gameById } from '../games.selectors';
 import { loadGame, forceEndGame, reinitializeServer } from '../games.actions';
 import { Player } from '@app/players/models/player';
@@ -13,10 +13,10 @@ import { loadPlayer } from '@app/players/actions';
 import { profile } from '@app/profile/profile.selectors';
 import { Title } from '@angular/platform-browser';
 import { environment } from '@environment';
+import { GamePlayer } from '../models/game-player';
+import { GamesService } from '../games.service';
 
-interface PlayerWithGameClass extends Player {
-  gameClass: string;
-}
+type ResolvedGamePlayer = Player & GamePlayer & { classSkill?: number };
 
 @Component({
   selector: 'app-game-details',
@@ -24,12 +24,14 @@ interface PlayerWithGameClass extends Player {
   styleUrls: ['./game-details.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class GameDetailsComponent implements OnInit {
+export class GameDetailsComponent implements OnInit, OnDestroy {
 
+  private destroyed = new Subject<void>();
   private audio = new Audio('/assets/sounds/fight.wav');
+  private players = new ReplaySubject<ResolvedGamePlayer[]>(1);
   game: Observable<Game>;
-  playersRed: Observable<PlayerWithGameClass[]>;
-  playersBlu: Observable<PlayerWithGameClass[]>;
+  playersRed: Observable<ResolvedGamePlayer[]>;
+  playersBlu: Observable<ResolvedGamePlayer[]>;
   isAdmin: Observable<boolean> = this.store.pipe(
     select(profile),
     map(theProfile => theProfile && (theProfile.role === 'super-user' || theProfile.role === 'admin')),
@@ -42,57 +44,93 @@ export class GameDetailsComponent implements OnInit {
     private route: ActivatedRoute,
     private store: Store<AppState>,
     private title: Title,
+    private gamesService: GamesService,
   ) { }
 
   ngOnInit() {
-    this.game = this.route.paramMap.pipe(
+    const getGameId = this.route.paramMap.pipe(
       map(params => params.get('id')),
-      switchMap(id => this.store.select(gameById(id)).pipe(
+    );
+
+    // retrieve the game
+    this.game = getGameId.pipe(
+      switchMap(gameId => this.store.select(gameById(gameId)).pipe(
         tap(game => {
           if (!game) {
-            this.store.dispatch(loadGame({ gameId: id }));
+            this.store.dispatch(loadGame({ gameId }));
           } else {
             this.title.setTitle(`Pickup #${game.number} • ${environment.titleSuffix}`);
           }
         }),
       )),
       filter(game => !!game),
-      tap(game => {
-        const playersForTeam = (teamId: string) => zip(
-          ...game.slots
-            .filter(p => p.teamId === teamId)
-            .map(slot =>
-              this.store.pipe(
-                select(playerById(slot.playerId)),
-                tap(player => {
-                  if (!player) {
-                    this.store.dispatch(loadPlayer({ playerId: slot.playerId }));
-                  }
-                }),
-                filter(player => !!player),
-                map(player => ({ ...player, gameClass: slot.gameClass })),
-              )
-            )
-        );
-
-        const redTeamId = Object.keys(game.teams).find(key => game.teams[key] === 'RED');
-        this.playersRed = playersForTeam(redTeamId);
-        const bluTeamId = Object.keys(game.teams).find(key => game.teams[key] === 'BLU');
-        this.playersBlu = playersForTeam(bluTeamId);
-      }),
+      shareReplay(),
     );
+
+    // resolve game slot into a real player
+    const resolveGamePlayer = (slot: GamePlayer) => this.store.pipe(
+      select(playerById(slot.playerId)),
+      tap(player => {
+        if (!player) {
+          this.store.dispatch(loadPlayer({ playerId: slot.playerId }));
+        }
+      }),
+      filter(player => !!player),
+      map(player => ({ ...player, ...slot })),
+    );
+
+    // load all players
+    this.game.pipe(
+      switchMap(game => combineLatest([
+        ...game.slots.map(slot => resolveGamePlayer(slot))
+      ])),
+      takeUntil(this.destroyed),
+    ).subscribe(players => this.players.next(players));
+
+    // get team id for the given team name
+    const getTeamId = (teamName: string) => this.game.pipe(
+      map(game => Object.keys(game.teams).find(key => game.teams[key] === teamName)),
+    );
+
+    // divide players into red and blu
+    this.playersRed = combineLatest([this.players, getTeamId('RED')]).pipe(
+      map(([allPlayers, redTeamId]) => allPlayers.filter(p => p.teamId === redTeamId)),
+    );
+
+    this.playersBlu = combineLatest([this.players, getTeamId('BLU')]).pipe(
+      map(([allPlayers, bluTeamId]) => allPlayers.filter(p => p.teamId === bluTeamId)),
+    );
+
+    // fetch game skills if the current user is an admin
+    this.isAdmin.pipe(
+      filter(isAdmin => isAdmin),
+      switchMap(() => getGameId),
+      switchMap(gameId => this.gamesService.fetchGameSkills(gameId)),
+      filter(skills => !!skills),
+      withLatestFrom(this.players),
+      takeUntil(this.destroyed),
+    ).subscribe(([skills, players]) => {
+      const playersWithSkills = players.map(player => ({ ...player, classSkill: skills[player.id] || undefined }));
+      this.players.next(playersWithSkills);
+    });
 
     // play sound when the connect is available
     this.game.pipe(
       map(game => game.connectString),
       pairwise(),
       filter(([a, b])  => !a && !!b),
+      takeUntil(this.destroyed),
     ).subscribe(() => {
       this.audio.play();
       const notification = new Notification('Join the game', {
         body: 'The server is ready. Join the game!',
       });
     });
+  }
+
+  ngOnDestroy() {
+    this.destroyed.next();
+    this.destroyed.unsubscribe();
   }
 
   copyConnectString() {
