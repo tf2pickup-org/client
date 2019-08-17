f36ffb69f219b1d4356418a8b5aa528f98116768
Michał Garapich <michal@garapich.pl>
2019-08-18 01:26:39 +0200

    bring back the old QueueStatusComponent

--- src/app/queue/queue-status/queue-status.component.ts
+++ src/app/queue/queue-status/queue-status.component.ts
@@ -1,12 +1,10 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { Observable, combineLatest } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/app.state';
-import { queueCurrentPlayerCount, queueRequiredPlayerCount, queueState, queueMap, isInQueue,
-    mapChangeVoterCount, queueConfig, votesForMapChange } from '../queue.selectors';
+import { queueCurrentPlayerCount, queueRequiredPlayerCount, queueState, queueMap } from '../queue.selectors';
 import { QueueState } from '../models/queue-state';
-import { toggleVoteForMapChange } from '../queue.actions';
-import { map, filter, debounceTime } from 'rxjs/operators';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-queue-status',
@@ -18,26 +16,11 @@ export class QueueStatusComponent {
 
   playerCount: Observable<number> = this.store.select(queueCurrentPlayerCount).pipe(debounceTime(100));
   requiredPlayerCount: Observable<number> = this.store.select(queueRequiredPlayerCount);
-  state: Observable<QueueState>  = this.store.select(queueState);
+  state = this.store.select(queueState);
   map: Observable<string> = this.store.select(queueMap);
-  isInQueue: Observable<boolean> = this.store.select(isInQueue).pipe(debounceTime(100));
-  mapChangeVoteTooltipText: Observable<string> =
-    combineLatest([
-      this.store.select(mapChangeVoterCount).pipe(debounceTime(100)),
-      this.store.select(queueConfig).pipe(filter(config => !!config)),
-    ]).pipe(
-      map(([count, config]) => `${count}/${config.nextMapSuccessfulVoteThreshold}`),
-    );
-    votesForMapChange: Observable<boolean> = this.store.select(votesForMapChange);
-
-  votes = false;
 
   constructor(
     private store: Store<AppState>,
   ) { }
-
-  voteForMapChangeToggle() {
-    this.store.dispatch(toggleVoteForMapChange());
-  }
 
 }
