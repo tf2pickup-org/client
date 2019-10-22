26090664d327cbb126242b88a4933bde6d1411c0
Michał <michal@garapich.pl>
2019-10-22 15:44:00 +0200

    fix VoteForMapChangeButtonComponent test

--- src/app/queue/vote-for-map-change-button/vote-for-map-change-button.component.spec.ts
+++ src/app/queue/vote-for-map-change-button/vote-for-map-change-button.component.spec.ts
@@ -1,9 +1,9 @@
-import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { VoteForMapChangeButtonComponent } from './vote-for-map-change-button.component';
 import { provideMockStore, MockStore } from '@ngrx/store/testing';
 import { AppState } from '@app/app.state';
 import { MemoizedSelector, Store } from '@ngrx/store';
-import { isInQueue, votesForMapChange, mapChangeVoterCount, queueConfig } from '../queue.selectors';
+import { isInQueue, mapChangeVoterCount, queueConfig } from '../queue.selectors';
 import { By } from '@angular/platform-browser';
 
 describe('VoteForMapChangeButtonComponent', () => {
@@ -11,7 +11,6 @@ describe('VoteForMapChangeButtonComponent', () => {
   let fixture: ComponentFixture<VoteForMapChangeButtonComponent>;
   let store: MockStore<any>;
   let fakeIsInQueue: MemoizedSelector<AppState, boolean>;
-  let fakeVotesForMapChange: MemoizedSelector<AppState, boolean>;
   let theButton: HTMLButtonElement;
 
   beforeEach(async(() => {
@@ -32,7 +31,6 @@ describe('VoteForMapChangeButtonComponent', () => {
   beforeEach(() => {
     store = TestBed.get(Store);
     fakeIsInQueue = store.overrideSelector(isInQueue, false);
-    fakeVotesForMapChange = store.overrideSelector(votesForMapChange, false);
 
     fixture = TestBed.createComponent(VoteForMapChangeButtonComponent);
     component = fixture.componentInstance;
@@ -50,11 +48,9 @@ describe('VoteForMapChangeButtonComponent', () => {
       expect(theButton.disabled).toBe(true);
     });
 
-    xit('should enable the button if he is', () => {
-      // this should be working, but setResult() doesn't work as it should
-      // https://github.com/ngrx/platform/issues/2000
+    it('should enable the button if he is', () => {
       fakeIsInQueue.setResult(true);
-      store.setState({ });
+      store.refreshState();
       fixture.detectChanges();
       expect(theButton.disabled).toBe(false);
     });
