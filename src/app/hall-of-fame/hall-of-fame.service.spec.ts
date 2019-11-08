a2581eb7991c355c2209c2cfe94846dd95506d89
Michał <michal@garapich.pl>
2019-11-08 17:50:04 +0100

    add tests

--- src/app/hall-of-fame/hall-of-fame.service.spec.ts
+++ src/app/hall-of-fame/hall-of-fame.service.spec.ts
@@ -1,12 +1,33 @@
-import { TestBed } from '@angular/core/testing';
-
+import { TestBed, inject } from '@angular/core/testing';
 import { HallOfFameService } from './hall-of-fame.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { API_URL } from '@app/api-url';
 
 describe('HallOfFameService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let httpContoller: HttpTestingController;
+
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientTestingModule,
+    ],
+    providers: [
+      { provide: API_URL, useValue: 'FAKE_URL' },
+    ],
+  }));
+
+  beforeEach(() => httpContoller = TestBed.get(HttpTestingController));
+  afterEach(() => httpContoller.verify());
 
   it('should be created', () => {
     const service: HallOfFameService = TestBed.get(HallOfFameService);
     expect(service).toBeTruthy();
+  });
+
+  describe('#fetchHallOfFames()', () => {
+    it('should call the endpoint', inject([HallOfFameService], (service: HallOfFameService) => {
+      service.fetchHallOfFames().subscribe();
+      httpContoller.expectOne('FAKE_URL/hall-of-fame');
+      expect().nothing();
+    }));
   });
 });
--- src/app/hall-of-fame/hall-of-fame/hall-of-fame.component.spec.ts
+++ src/app/hall-of-fame/hall-of-fame/hall-of-fame.component.spec.ts
@@ -1,6 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HallOfFameComponent } from './hall-of-fame.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { API_URL } from '@app/api-url';
+import { Title } from '@angular/platform-browser';
+import { NEVER } from 'rxjs';
+import { HallOfFameService } from '../hall-of-fame.service';
+
+class TitleStub {
+  setTitle(title: string) { }
+}
+
+class HallOfFameServiceStub {
+  fetchHallOfFames() { return NEVER; }
+}
 
 describe('HallOfFameComponent', () => {
   let component: HallOfFameComponent;
@@ -8,7 +22,17 @@ describe('HallOfFameComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HallOfFameComponent ]
+      declarations: [ HallOfFameComponent ],
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+      ],
+      providers: [
+        { provide: API_URL, useValue: 'FAKE_URL' },
+        { provide: Title, useClass: TitleStub },
+        { provide: HallOfFameService, useClass: HallOfFameServiceStub },
+      ],
+      schemas: [ NO_ERRORS_SCHEMA ],
     })
     .compileComponents();
   }));
@@ -21,5 +45,19 @@ describe('HallOfFameComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('#ngOnInit()', () => {
+    it('should set title', () => {
+      const spy = spyOn(TestBed.get(Title), 'setTitle');
+      component.ngOnInit();
+      expect(spy).toHaveBeenCalledWith(jasmine.any(String));
+    });
+
+    it('should call HallOfFameService.fetchHallOfFame()', () => {
+      const spy = spyOn(TestBed.get(HallOfFameService), 'fetchHallOfFames');
+      component.ngOnInit();
+      expect(spy).toHaveBeenCalled();
+    });
   });
 });
