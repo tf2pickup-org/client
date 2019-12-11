c7c07773a0d968fb410cb4234dd9a2740fbb8ab2
Michał <michal@garapich.pl>
2019-12-11 13:19:00 +0100

    setup environment

--- src/environments/tf2pickup.pl.ts
+++ src/environments/tf2pickup.pl.ts
@@ -1,19 +1,14 @@
 import { version } from 'package.json';
 
-const discordInvitation = 'https://discord.gg/UVFVfc4';
-
 export const environment = {
   production: true,
-  apiUrl: 'https://api.tf2pickup.pl',
-  wsUrl: 'https://api.tf2pickup.pl',
+  apiUrl: 'https://api.tf2pickup.es',
+  wsUrl: 'https://api.tf2pickup.es',
   version,
-  headerLinks: [
-    { name: 'discord', icon: '/assets/discord.svg', target: discordInvitation, tooltip: 'Join us on discord!' },
-  ],
+  headerLinks: [],
   footerLinks: [
     { name: 'github', target: 'https://github.com/tf2pickup-pl' },
-    { name: 'discord', target: discordInvitation },
     { name: 'changelog', target: 'https://github.com/tf2pickup-pl/client/blob/master/CHANGELOG.md' },
   ],
-  titleSuffix: 'tf2pickup.pl • Polskie Pickupy',
+  titleSuffix: 'tf2pickup.es • Spanish Team Fortress 2 Pickup-up games',
 };
