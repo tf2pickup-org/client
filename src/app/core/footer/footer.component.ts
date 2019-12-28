073c3ff62667ab7ec06a4755a5fcd1b5fa201736
Michał Garapich <michal@garapich.pl>
2019-12-28 13:47:01 +0100

    FooterComponent: add skilltable link

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -2,6 +2,9 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { TokenStoreService } from '@app/auth/token-store.service';
 import { AuthService } from '@app/auth/auth.service';
+import { Store } from '@ngrx/store';
+import { isAdmin } from '@app/profile/profile.selectors';
+import { AppState } from '@app/app.state';
 
 @Component({
   selector: 'app-footer',
@@ -14,10 +17,12 @@ export class FooterComponent {
   version = environment.version;
   links = environment.footerLinks;
   isAuthenticated = this.authService.authenticated;
+  isAdmin = this.store.select(isAdmin);
 
   constructor(
     private tokenStore: TokenStoreService,
     private authService: AuthService,
+    private store: Store<AppState>,
   ) { }
 
   logout() {
