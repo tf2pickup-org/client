3d279965d3f976bec60944ba88854d86c17cb6af
Michał Garapich <michal@garapich.pl>
2019-12-28 23:01:35 +0100

    GameServerListComponent test update

--- src/app/game-servers/game-server-list/game-server-list.component.spec.ts
+++ src/app/game-servers/game-server-list/game-server-list.component.spec.ts
@@ -5,15 +5,19 @@ import { provideMockStore, MockStore } from '@ngrx/store/testing';
 import { AppState } from '@app/app.state';
 import { Store } from '@ngrx/store';
 import { allGameServers } from '../game-servers.selectors';
+import { removeGameServer, loadGameServer, loadGameServers } from '../game-servers.actions';
 
 class BsModalServiceStub {
-
+  show() { }
 }
 
 describe('GameServerListComponent', () => {
   let component: GameServerListComponent;
   let fixture: ComponentFixture<GameServerListComponent>;
   let store: MockStore<AppState>;
+  let bsModalService: BsModalServiceStub;
+  let storeDispatchSpy: jasmine.Spy;
+
   const initialState = {
     profile: { },
     gameServers: {
@@ -33,6 +37,7 @@ describe('GameServerListComponent', () => {
     .compileComponents();
 
     store = TestBed.get(Store);
+    storeDispatchSpy = spyOn(store, 'dispatch').and.callThrough();
     store.overrideSelector(allGameServers, []);
   }));
 
@@ -40,9 +45,33 @@ describe('GameServerListComponent', () => {
     fixture = TestBed.createComponent(GameServerListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    bsModalService = TestBed.get(BsModalService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('#ngOnInit()', () => {
+    it('should load all the game servers', () => {
+      component.ngOnInit();
+      expect(storeDispatchSpy).toHaveBeenCalledWith(loadGameServers());
+    });
+  });
+
+  describe('#addGameServer()', () => {
+    it('should open the modal dialog', () => {
+      const spy = spyOn(bsModalService, 'show');
+      component.addGameServer();
+      expect(spy).toHaveBeenCalled();
+    });
+  });
+
+  describe('#removeGameServer()', () => {
+    it('should dispatch an action', () => {
+      component.removeGameServer({ id: 'FAKE_ID' } as any);
+      expect(storeDispatchSpy).toHaveBeenCalledWith(removeGameServer({ gameServerId: 'FAKE_ID' }));
+    });
   });
 });
