3f0a246e22a17d9e8350fdb7fa83d43b33de4e45
Michał Garapich <michal@garapich.pl>
2020-02-16 04:05:00 +0100

    add tf2ClassPriority

---
+++ src/app/games/tf2-class-priority.ts
@@ -0,0 +1,14 @@
+/**
+ * Orders classes in the game slot list.
+ */
+export const tf2ClassPriority = new Map([
+  [ 'scout',    9 ],
+  [ 'soldier',  8 ],
+  [ 'pyro',     7 ],
+  [ 'demoman',  6 ],
+  [ 'heavy',    5 ],
+  [ 'engineer', 4 ],
+  [ 'medic',    3 ],
+  [ 'sniper',   2 ],
+  [ 'spy',      1 ],
+]);
