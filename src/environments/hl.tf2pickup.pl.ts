56e57d4b5fdd444e0739a5cac14bb7502128a3fe
Michał Garapich <michal@garapich.pl>
2020-05-02 16:15:08 +0200

    add hl.tf2pickup.pl config

---
+++ src/environments/hl.tf2pickup.pl.ts
@@ -0,0 +1,19 @@
+import { version } from 'package.json';
+
+const discordInvitation = 'https://discord.gg/UVFVfc4';
+
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.hl.tf2pickup.pl',
+  wsUrl: 'https://api.hl.tf2pickup.pl',
+  version,
+  headerLinks: [
+    { name: 'discord', icon: '/assets/discord.svg', target: discordInvitation, tooltip: 'Join us on discord!' },
+  ],
+  footerLinks: [
+    { name: 'github', target: 'https://github.com/tf2pickup-pl' },
+    { name: 'discord', target: discordInvitation },
+    { name: 'changelog', target: 'https://github.com/tf2pickup-pl/client/blob/master/CHANGELOG.md' },
+  ],
+  titleSuffix: 'hl.tf2pickup.pl • Polskie Pickupy 9v9',
+};
