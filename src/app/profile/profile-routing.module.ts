a7015be178f7e718b149d019b20ed665aa18606b
Michał Garapich <michal@garapich.pl>
2020-05-08 14:49:44 +0200

    add SettingsComponent

---
+++ src/app/profile/profile-routing.module.ts
@@ -0,0 +1,13 @@
+import { Routes, RouterModule } from '@angular/router';
+import { SettingsComponent } from './settings/settings.component';
+import { NgModule } from '@angular/core';
+
+const routes: Routes = [
+  { path: 'settings', component: SettingsComponent, data: { title: 'settings' } },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ProfileRoutingModule { }
---
+++ src/app/profile/settings/settings.component.ts
@@ -0,0 +1,9 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-settings',
+  templateUrl: './settings.component.html',
+  styleUrls: ['./settings.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SettingsComponent { }
