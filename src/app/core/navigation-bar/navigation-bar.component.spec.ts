63eee0126836eed11602ba259a51e079d0f2b46a
Michał Garapich <michal@garapich.pl>
2020-05-09 14:20:21 +0200

    feat: settings page (#251)
    * add SettingsComponent
    
    * tests update

--- src/app/core/navigation-bar/navigation-bar.component.spec.ts
+++ src/app/core/navigation-bar/navigation-bar.component.spec.ts
@@ -1,14 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NavigationBarComponent } from './navigation-bar.component';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { NO_ERRORS_SCHEMA, ChangeDetectionStrategy } from '@angular/core';
 import { AuthService } from '@app/auth/auth.service';
 import { provideMockStore, MockStore } from '@ngrx/store/testing';
 import { By } from '@angular/platform-browser';
 import { Store } from '@ngrx/store';
 
-class AuthServiceStub {
-  authenticated = false;
-}
+const authServiceStub = {
+  authenticated: false,
+};
 
 describe('NavigationBarComponent', () => {
   let component: NavigationBarComponent;
@@ -20,11 +20,13 @@ describe('NavigationBarComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ NavigationBarComponent ],
       providers: [
-        { provide: AuthService, useClass: AuthServiceStub },
+        { provide: AuthService, useValue: authServiceStub },
         provideMockStore({ initialState }),
       ],
       schemas: [ NO_ERRORS_SCHEMA ],
     })
+    // https://github.com/angular/angular/issues/12313
+    .overrideComponent(NavigationBarComponent, { set: { changeDetection: ChangeDetectionStrategy.Default } })
     .compileComponents();
   }));
 
@@ -45,16 +47,26 @@ describe('NavigationBarComponent', () => {
     expect(el).toBeTruthy();
   });
 
+  describe('when logged in', () => {
+    beforeEach(() => {
+      store.setState({ profile: { name: 'FAKE_NAME', id: 'FAKE_ID' } });
+      component.isAuthenticated = true;
+      fixture.detectChanges();
+    });
+
+    it('should render profile link', () => {
+      const el = fixture.debugElement.query(By.css('.profile-link a')).nativeElement as HTMLAnchorElement;
+      expect(el).toBeTruthy();
+      expect(el.innerText.trim()).toBe('FAKE_NAME');
+    });
+
+    it('should render link to the settings page', () => {
+      expect(fixture.debugElement.query(By.css('a[routerLink="/settings"]'))).toBeTruthy();
+    });
+  });
+
   it('app-steam-login-button should be rendered if user is not authenticated', () => {
     const el = fixture.debugElement.query(By.css('app-steam-login-button'));
     expect(el).toBeTruthy();
-  });
-
-  it('should render profile link', () => {
-    store.setState({ profile: { name: 'FAKE_NAME', id: 'FAKE_ID' } });
-    fixture.detectChanges();
-    const el = fixture.debugElement.query(By.css('.profile-link a')).nativeElement as HTMLAnchorElement;
-    expect(el).toBeTruthy();
-    expect(el.innerText.trim()).toBe('FAKE_NAME');
   });
 });
--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -6,16 +6,21 @@ import { EffectsModule } from '@ngrx/effects';
 import { ProfileEffects } from './profile.effects';
 import { AcceptRulesDialogComponent } from './accept-rules-dialog/accept-rules-dialog.component';
 import { MarkdownModule } from 'ngx-markdown';
+import { SettingsComponent } from './settings/settings.component';
+import { ProfileRoutingModule } from './profile-routing.module';
 
 @NgModule({
   declarations: [
     AcceptRulesDialogComponent,
+    SettingsComponent,
   ],
   imports: [
     CommonModule,
     StoreModule.forFeature('profile', reducer),
     EffectsModule.forFeature([ProfileEffects]),
     MarkdownModule.forChild(),
+
+    ProfileRoutingModule,
   ],
 })
 export class ProfileModule { }
