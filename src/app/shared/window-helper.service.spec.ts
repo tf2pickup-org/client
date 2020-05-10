ccc1971eededd9352c361bea1724c1601f58e76a
Michał Garapich <michal@garapich.pl>
2020-05-11 01:55:42 +0200

    update WindowHelperService test

--- src/app/shared/window-helper.service.spec.ts
+++ src/app/shared/window-helper.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
 import { WindowHelperService } from './window-helper.service';
 
 describe('WindowHelperService', () => {
@@ -12,5 +11,13 @@ describe('WindowHelperService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('#openWindow()', () => {
+    it('should attempt to open a window with the specified params', () => {
+      const spy = spyOn(window, 'open');
+      service.openWindow({ url: 'FAKE_URL', width: 100, height: 100 });
+      expect(spy).toHaveBeenCalledWith('FAKE_URL', '', jasmine.any(String));
+    });
   });
 });
