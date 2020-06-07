0cae755164ab04e0522f24f9f8c7390550bf930b
Michal Garapich <michal@garapich.pl>
2020-06-07 13:48:39 +0200

    QueueSlotItem: leave button

--- src/app/icons/icons.module.ts
+++ src/app/icons/icons.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
-import { Eye, Plus } from 'angular-feather/icons';
+import { Eye, Minus, Plus } from 'angular-feather/icons';
 import { FeatherModule } from 'angular-feather';
 
 // https://github.com/michaelbazos/angular-feather#available-icons
 const icons = {
   Eye,
+  Minus,
   Plus,
 };
 
