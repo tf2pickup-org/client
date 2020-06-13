2d311f242b1658df35982ed8d3586adbab084a87
Michal Garapich <michal@garapich.pl>
2020-06-13 02:20:46 +0200

    mark as friend button

--- src/app/icons/icons.module.ts
+++ src/app/icons/icons.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
-import { Eye, Minus, Plus } from 'angular-feather/icons';
+import { Eye, Heart, Minus, Plus } from 'angular-feather/icons';
 import { FeatherModule } from 'angular-feather';
 
 // https://github.com/michaelbazos/angular-feather#available-icons
 const icons = {
   Eye,
+  Heart,
   Minus,
   Plus,
 };
