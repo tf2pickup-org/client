98174a5019ba0e0b001bf9ceeb87f740d94fa425
Michał Garapich <michal@garapich.pl>
2020-10-14 02:04:05 +0200

    fix: export global var (#439)

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,6 +1,7 @@
 /***************************************************************************************************
  * BROWSER POLYFILLS
  */
+import './global-shim';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
