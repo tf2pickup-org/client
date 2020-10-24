d6400656e4e95556e1f731bbde7637e06b674767
Michał Garapich <michal@garapich.pl>
2020-10-24 15:15:58 +0200

    update AcceptRulesDialogService test

--- src/app/profile/accept-rules-dialog.service.spec.ts
+++ src/app/profile/accept-rules-dialog.service.spec.ts
@@ -1,12 +1,16 @@
+import { OverlayModule } from '@angular/cdk/overlay';
 import { TestBed } from '@angular/core/testing';
-
 import { AcceptRulesDialogService } from './accept-rules-dialog.service';
 
 describe('AcceptRulesDialogService', () => {
   let service: AcceptRulesDialogService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        OverlayModule,
+      ],
+    });
     service = TestBed.inject(AcceptRulesDialogService);
   });
 
