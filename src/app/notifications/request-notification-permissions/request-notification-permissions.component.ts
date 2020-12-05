b7426ce5270d6d3a74570f2637336b553db8c302
Michał Garapich <michal@garapich.pl>
2020-12-05 03:39:06 +0100

    animations initial

--- src/app/notifications/request-notification-permissions/request-notification-permissions.component.ts
+++ src/app/notifications/request-notification-permissions/request-notification-permissions.component.ts
@@ -1,3 +1,4 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { NotificationsService } from '../notifications.service';
 
@@ -6,6 +7,20 @@ import { NotificationsService } from '../notifications.service';
   templateUrl: './request-notification-permissions.component.html',
   styleUrls: ['./request-notification-permissions.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    trigger('fadeOut', [
+      transition(':leave', [
+        animate('400ms ease-in-out', style({
+          opacity: 0,
+          height: 0,
+          paddingTop: 0,
+          paddingBottom: 0,
+          marginTop: 0,
+          marginBottom: 0,
+        })),
+      ]),
+    ]),
+  ],
 })
 export class RequestNotificationPermissionsComponent {
 
