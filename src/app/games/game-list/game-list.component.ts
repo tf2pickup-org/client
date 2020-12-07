9184974ed57e4d9b8602544c48161dbf5beb8d31
Michał Garapich <michal@garapich.pl>
2020-12-07 13:55:13 +0100

    GameListComponent fine-tuning

--- src/app/games/game-list/game-list.component.ts
+++ src/app/games/game-list/game-list.component.ts
@@ -1,47 +1,25 @@
-import { Component, ChangeDetectionStrategy, OnInit, OnDestroy } from '@angular/core';
-import { ReplaySubject, BehaviorSubject, Subject } from 'rxjs';
-import { Game } from '../models/game';
-import { GamesService } from '../games.service';
-import { switchMap, map, takeUntil } from 'rxjs/operators';
+import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import { GameListStore } from './game-list.store';
 
 @Component({
   selector: 'app-game-list',
   templateUrl: './game-list.component.html',
   styleUrls: ['./game-list.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [ GameListStore ],
 })
-export class GameListComponent implements OnInit, OnDestroy {
-
-  readonly gamesPerPage = 5;
-
-  page = new BehaviorSubject<number>(1);
-  gameCount = new ReplaySubject<number>(1);
-  games = new ReplaySubject<Game[]>(1);
-
-  private destroyed = new Subject<void>();
+export class GameListComponent implements OnInit {
 
   constructor(
-    private gamesService: GamesService,
+    public readonly store: GameListStore,
   ) { }
 
   ngOnInit() {
-    this.page.pipe(
-      map(page => page - 1),
-      switchMap(page => this.gamesService.fetchGames(page * this.gamesPerPage, this.gamesPerPage)),
-      takeUntil(this.destroyed),
-    ).subscribe(response => {
-      this.gameCount.next(response.itemCount);
-      this.games.next(response.results);
-    });
+    this.loadPage(1);
   }
 
-  getPage(page: number) {
-    this.page.next(page);
-  }
-
-  ngOnDestroy() {
-    this.destroyed.next();
-    this.destroyed.complete();
+  loadPage(page: number) {
+    this.store.loadPage(page);
   }
 
 }
