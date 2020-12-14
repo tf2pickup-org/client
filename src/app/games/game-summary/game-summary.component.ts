bb7450c5a9528b6dfda8cd6198cedc96df85f14c
Michał Garapich <michal@garapich.pl>
2020-12-14 02:06:15 +0100

    GameSummaryComponent: add fadeIn animation

--- src/app/games/game-summary/game-summary.component.ts
+++ src/app/games/game-summary/game-summary.component.ts
@@ -1,10 +1,17 @@
+import { transition, trigger, useAnimation } from '@angular/animations';
 import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+import { fadeIn } from '@app/animations';
 
 @Component({
   selector: 'app-game-summary',
   templateUrl: './game-summary.component.html',
   styleUrls: ['./game-summary.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', useAnimation(fadeIn)),
+    ]),
+  ],
 })
 export class GameSummaryComponent {
 
