94c6849fd28e6056f81674672702a849ba2a2b58
Michał Garapich <michal@garapich.pl>
2020-12-14 09:53:14 +0100

    TwitchTvIntegrationComponent: update changeDetection strategy

--- src/app/profile/twitch-tv-integration/twitch-tv-integration.component.ts
+++ src/app/profile/twitch-tv-integration/twitch-tv-integration.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TwitchService } from '@app/twitch/twitch.service';
 import { Store } from '@ngrx/store';
 import { filter, map } from 'rxjs/operators';
@@ -8,6 +8,7 @@ import { twitchTvUser } from '../profile.selectors';
   selector: 'app-twitch-tv-integration',
   templateUrl: './twitch-tv-integration.component.html',
   styleUrls: ['./twitch-tv-integration.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TwitchTvIntegrationComponent {
 
