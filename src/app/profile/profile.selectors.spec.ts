5a8f8306e53875536b0a9c350e60b23d097f865f
Michał Garapich <michal@garapich.pl>
2020-12-15 01:36:12 +0100

    fix: canSubstituteInGame selector returns true when the user is not logged in (#525)

--- src/app/profile/profile.selectors.spec.ts
+++ src/app/profile/profile.selectors.spec.ts
@@ -1,47 +1,59 @@
-import { isAdmin, isSuperUser, bans, isBanned, twitchTvUser } from './profile.selectors';
+import { isAdmin, isSuperUser, bans, isBanned, twitchTvUser, isLoggedIn } from './profile.selectors';
 
-describe('profile selectors', () => {
-  describe('isAdmin', () => {
-    it('should return the correct value', () => {
-      expect(isAdmin.projector(null)).toBeFalsy();
-      expect(isAdmin.projector({ })).toBe(false);
-      expect(isAdmin.projector({ role: 'whatever' })).toBe(false);
-      expect(isAdmin.projector({ role: 'admin' })).toBe(true);
-      expect(isAdmin.projector({ role: 'super-user' })).toBe(true);
+describe('isLoggedIn', () => {
+  describe('when the user is logged in', () => {
+    it('should return true', () => {
+      expect(isLoggedIn.projector({ })).toBe(true);
     });
   });
 
-  describe('isSuperUser', () => {
-    it('should return the correct value', () => {
-      expect(isSuperUser.projector(null)).toBeFalsy();
-      expect(isSuperUser.projector({ })).toBe(false);
-      expect(isSuperUser.projector({ role: 'something' })).toBe(false);
-      expect(isSuperUser.projector({ role: 'super-user' })).toBe(true);
-      expect(isSuperUser.projector({ role: 'admin' })).toBe(false);
+  describe('when the user is not logged in', () => {
+    it('should return false', () => {
+      expect(isLoggedIn.projector(null)).toBe(false);
     });
   });
+});
 
-  describe('bans', () => {
-    it('should return player bans', () => {
-      expect(bans.projector(undefined)).toBeUndefined();
-      expect(bans.projector(null)).toBeUndefined();
-      expect(bans.projector({ bans: [] })).toEqual([]);
-    });
+describe('isAdmin', () => {
+  it('should return the correct value', () => {
+    expect(isAdmin.projector(null)).toBeFalsy();
+    expect(isAdmin.projector({ })).toBe(false);
+    expect(isAdmin.projector({ role: 'whatever' })).toBe(false);
+    expect(isAdmin.projector({ role: 'admin' })).toBe(true);
+    expect(isAdmin.projector({ role: 'super-user' })).toBe(true);
   });
+});
 
-  describe('isBanned', () => {
-    it('should return true if the player has at least one ban', () => {
-      expect(isBanned.projector(null)).toBe(false);
-      expect(isBanned.projector([])).toBe(false);
-      expect(isBanned.projector([ { } ])).toBe(true);
-    });
+describe('isSuperUser', () => {
+  it('should return the correct value', () => {
+    expect(isSuperUser.projector(null)).toBeFalsy();
+    expect(isSuperUser.projector({ })).toBe(false);
+    expect(isSuperUser.projector({ role: 'something' })).toBe(false);
+    expect(isSuperUser.projector({ role: 'super-user' })).toBe(true);
+    expect(isSuperUser.projector({ role: 'admin' })).toBe(false);
   });
+});
 
-  describe('twitchTvUser', () => {
-    it('should return twitch.tv user profile', () => {
-      expect(twitchTvUser.projector(null)).toBe(undefined);
-      expect(twitchTvUser.projector({  })).toBe(undefined);
-      expect(twitchTvUser.projector({ twitchTvUser: { displayName: 'foo' } })).toEqual({ displayName: 'foo' } as any);
-    });
+describe('bans', () => {
+  it('should return player bans', () => {
+    expect(bans.projector(undefined)).toBeUndefined();
+    expect(bans.projector(null)).toBeUndefined();
+    expect(bans.projector({ bans: [] })).toEqual([]);
+  });
+});
+
+describe('isBanned', () => {
+  it('should return true if the player has at least one ban', () => {
+    expect(isBanned.projector(null)).toBe(false);
+    expect(isBanned.projector([])).toBe(false);
+    expect(isBanned.projector([ { } ])).toBe(true);
+  });
+});
+
+describe('twitchTvUser', () => {
+  it('should return twitch.tv user profile', () => {
+    expect(twitchTvUser.projector(null)).toBe(undefined);
+    expect(twitchTvUser.projector({  })).toBe(undefined);
+    expect(twitchTvUser.projector({ twitchTvUser: { displayName: 'foo' } })).toEqual({ displayName: 'foo' } as any);
   });
 });
--- src/app/selectors.spec.ts
+++ src/app/selectors.spec.ts
@@ -1,106 +1,130 @@
 import { QueueSlot } from './queue/models/queue-slot';
 import { queueLocked, canSubstituteInGame, isReadyUpDialogShown } from './selectors';
 
-describe('root selectors', () => {
-  describe('queueLocked', () => {
-    it('should return true if the user is not logged in', () => {
+describe('queueLocked', () => {
+  describe('when the user is not logged in', () => {
+    it('should return true', () => {
       expect(queueLocked.projector(null, null)).toBe(true);
     });
+  });
 
-    it('should return true if the user has an active game', () => {
+  describe('when the user has an active game', () => {
+    it('should return true', () => {
       expect(queueLocked.projector({ }, { })).toBe(true);
     });
+  });
 
-    it('should return true if the user has active bans', () => {
-      expect(queueLocked.projector({ bans: [{}] }, null)).toBe(true);
+  describe('when the user has active bans', () => {
+    it('should return true', () => {
+      expect(queueLocked.projector({ bans: [{ }] }, null)).toBe(true);
+    });
+  });
+});
+
+describe('canSubstituteInGame', () => {
+  describe('when not logged in', () => {
+    it('should return false', () => {
+      expect(canSubstituteInGame('FAKE_GAME_ID').projector(false, true, false, null, { id: 'FAKE_GAME_ID' })).toBe(false);
     });
   });
 
-  describe('canSubstituteInGame', () => {
-    it('should return false if the given game is not running', () => {
-      expect(canSubstituteInGame('FAKE_GAME_ID').projector(false, false, null, null)).toBe(false);
-    });
-
-    it('should return false if the current user is banned', () => {
-      expect(canSubstituteInGame('FAKE_GAME_ID').projector(true, true, null, null)).toBe(false);
-    });
-
-    it('should return true if the current user is not playing any game', () => {
-      expect(canSubstituteInGame('FAKE_GAME_ID').projector(true, false, null, { id: 'FAKE_GAME_ID' })).toBe(true);
-    });
-
-    it('should return false if the current user is playing a game', () => {
-      expect(canSubstituteInGame('FAKE_GAME_ID').projector(true, false, { id: 'SOME_OTHER_GAME' }, { id: 'FAKE_GAME_ID' })).toBe(false);
-    });
-
-    it('should return true if player is trying to sub in the same game', () => {
-      expect(canSubstituteInGame('FAKE_GAME_ID').projector(true, false, { id: 'FAKE_GAME_ID' }, { id: 'FAKE_GAME_ID' })).toBe(true);
-    });
-  });
-
-  describe('isReadyUpDialogShown', () => {
-    let mySlot: QueueSlot;
-
-    describe('when the user is in the queue', () => {
-      beforeEach(() => {
-        mySlot = { id: 0, gameClass: 'soldier', playerId: 'FAKE_PLAYER_ID', ready: false };
+  describe('when logged in', () => {
+    describe('when the given game is not runnong', () => {
+      it('should return false', () => {
+        expect(canSubstituteInGame('FAKE_GAME_ID').projector(true, false, false, null, null)).toBe(false);
       });
+    });
 
-      describe('when the queue is in ready state', () => {
-        describe('when the user is not ready', () => {
-          beforeEach(() => mySlot.ready = false);
+    describe('when the user is banned', () => {
+      it('should return false', () => {
+        expect(canSubstituteInGame('FAKE_GAME_ID').projector(true, true, true, null, null)).toBe(false);
+      });
+    });
 
-          describe('when is not pre-readied', () => {
-            it('should return true', () => {
-              expect(isReadyUpDialogShown.projector('ready', mySlot, false)).toBe(true);
-            });
-          });
+    describe('when the user is not playing any game', () => {
+      it('should return true', () => {
+        expect(canSubstituteInGame('FAKE_GAME_ID').projector(true, true, false, null, { id: 'FAKE_GAME_ID' })).toBe(true);
+      });
+    });
 
-          describe('when is pre-readied', () => {
-            it('should return false', () => {
-              expect(isReadyUpDialogShown.projector('ready', mySlot, true)).toBe(false);
-            });
+    describe('when the user is playing a game', () => {
+      it('should return false', () => {
+        expect(canSubstituteInGame('FAKE_GAME_ID').projector(true, true, false, { id: 'SOME_OTHER_GAME' }, { id: 'FAKE_GAME_ID' }))
+          .toBe(false);
+      });
+    });
+
+    describe('when the user is trying to sub in the same game', () => {
+      it('should return true', () => {
+        expect(canSubstituteInGame('FAKE_GAME_ID').projector(true, true, false, { id: 'FAKE_GAME_ID' }, { id: 'FAKE_GAME_ID' }))
+          .toBe(true);
+      });
+    });
+  });
+});
+
+describe('isReadyUpDialogShown', () => {
+  let mySlot: QueueSlot;
+
+  describe('when the user is in the queue', () => {
+    beforeEach(() => {
+      mySlot = { id: 0, gameClass: 'soldier', playerId: 'FAKE_PLAYER_ID', ready: false };
+    });
+
+    describe('when the queue is in ready state', () => {
+      describe('when the user is not ready', () => {
+        beforeEach(() => mySlot.ready = false);
+
+        describe('when is not pre-readied', () => {
+          it('should return true', () => {
+            expect(isReadyUpDialogShown.projector('ready', mySlot, false)).toBe(true);
           });
         });
 
-        describe('when the user is ready', () => {
-          beforeEach(() => mySlot.ready = true);
-
+        describe('when is pre-readied', () => {
           it('should return false', () => {
-            expect(isReadyUpDialogShown.projector('ready', mySlot, false)).toBe(false);
             expect(isReadyUpDialogShown.projector('ready', mySlot, true)).toBe(false);
           });
         });
       });
 
-      describe('when the queue is in waiting state', () => {
-        it('should return false', () => {
-          expect(isReadyUpDialogShown.projector('waiting', mySlot, false)).toBe(false);
-          expect(isReadyUpDialogShown.projector('waiting', mySlot, true)).toBe(false);
-        });
-      });
+      describe('when the user is ready', () => {
+        beforeEach(() => mySlot.ready = true);
 
-      describe('when the queue is in launching state', () => {
         it('should return false', () => {
-          expect(isReadyUpDialogShown.projector('launching', mySlot, false)).toBe(false);
-          expect(isReadyUpDialogShown.projector('launching', mySlot, true)).toBe(false);
+          expect(isReadyUpDialogShown.projector('ready', mySlot, false)).toBe(false);
+          expect(isReadyUpDialogShown.projector('ready', mySlot, true)).toBe(false);
         });
       });
     });
 
-    describe('when the user is not in the queue', () => {
-      beforeEach(() => {
-        mySlot = null;
-      });
-
+    describe('when the queue is in waiting state', () => {
       it('should return false', () => {
-        expect(isReadyUpDialogShown.projector('waiting', mySlot, true)).toBe(false);
         expect(isReadyUpDialogShown.projector('waiting', mySlot, false)).toBe(false);
-        expect(isReadyUpDialogShown.projector('ready', mySlot, true)).toBe(false);
-        expect(isReadyUpDialogShown.projector('ready', mySlot, false)).toBe(false);
-        expect(isReadyUpDialogShown.projector('launching', mySlot, true)).toBe(false);
-        expect(isReadyUpDialogShown.projector('launching', mySlot, false)).toBe(false);
+        expect(isReadyUpDialogShown.projector('waiting', mySlot, true)).toBe(false);
       });
+    });
+
+    describe('when the queue is in launching state', () => {
+      it('should return false', () => {
+        expect(isReadyUpDialogShown.projector('launching', mySlot, false)).toBe(false);
+        expect(isReadyUpDialogShown.projector('launching', mySlot, true)).toBe(false);
+      });
+    });
+  });
+
+  describe('when the user is not in the queue', () => {
+    beforeEach(() => {
+      mySlot = null;
+    });
+
+    it('should return false', () => {
+      expect(isReadyUpDialogShown.projector('waiting', mySlot, true)).toBe(false);
+      expect(isReadyUpDialogShown.projector('waiting', mySlot, false)).toBe(false);
+      expect(isReadyUpDialogShown.projector('ready', mySlot, true)).toBe(false);
+      expect(isReadyUpDialogShown.projector('ready', mySlot, false)).toBe(false);
+      expect(isReadyUpDialogShown.projector('launching', mySlot, true)).toBe(false);
+      expect(isReadyUpDialogShown.projector('launching', mySlot, false)).toBe(false);
     });
   });
 });
