cfe8e2dc29919e3afb091151f073c1f7546370fc
Michał Garapich <michal@garapich.pl>
2020-12-17 00:31:08 +0100

    fix: add missing crossorigin tags

--- src/app/players/player-avatar/player-avatar.component.spec.ts
+++ src/app/players/player-avatar/player-avatar.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { PlayerAvatarComponent } from './player-avatar.component';
 import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { Store } from '@ngrx/store';
+import { By } from '@angular/platform-browser';
 
 describe('PlayerAvatarComponent', () => {
   let component: PlayerAvatarComponent;
@@ -28,5 +29,10 @@ describe('PlayerAvatarComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render img with crossorigin="anonymous"', () => {
+    const imgs = fixture.debugElement.queryAll(By.css('img')).map(el => el.nativeElement) as HTMLImageElement[];
+    expect(imgs.every(img => img.crossOrigin === 'anonymous')).toBe(true);
   });
 });
--- src/app/players/player-details-header/player-details-header.component.spec.ts
+++ src/app/players/player-details-header/player-details-header.component.spec.ts
@@ -1,12 +1,12 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { PlayerDetailsHeaderComponent } from './player-details-header.component';
 
 describe('PlayerDetailsHeaderComponent', () => {
   let component: PlayerDetailsHeaderComponent;
   let fixture: ComponentFixture<PlayerDetailsHeaderComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PlayerDetailsHeaderComponent ]
     })
@@ -21,5 +21,10 @@ describe('PlayerDetailsHeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render img with crossorigin="anonymous"', () => {
+    const imgs = fixture.debugElement.queryAll(By.css('img')).map(el => el.nativeElement) as HTMLImageElement[];
+    expect(imgs.every(img => img.crossOrigin === 'anonymous')).toBe(true);
   });
 });
