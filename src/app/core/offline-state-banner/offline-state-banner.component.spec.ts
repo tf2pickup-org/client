97db7f00155669933cd0ea089ab2b47f5523d03d
Michał Garapich <michal@garapich.pl>
2021-01-09 19:53:10 +0100

    add OfflineStateBannerComponent

---
+++ src/app/core/offline-state-banner/offline-state-banner.component.spec.ts
@@ -0,0 +1,62 @@
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { ioConnected } from '@app/io/io.selectors';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { MockBuilder, MockedComponentFixture, MockRender, ngMocks } from 'ng-mocks';
+import { OfflineStateBannerComponent } from './offline-state-banner.component';
+
+describe(OfflineStateBannerComponent.name, () => {
+  let component: OfflineStateBannerComponent;
+  let fixture: MockedComponentFixture;
+  let store: MockStore;
+
+  beforeEach(() => MockBuilder(OfflineStateBannerComponent)
+    .provide(provideMockStore({
+      selectors: [
+        { selector: ioConnected, value: undefined },
+      ],
+    }))
+  );
+
+  beforeEach(() => {
+    fixture = MockRender(OfflineStateBannerComponent);
+    component = fixture.point.componentInstance;
+    fixture.detectChanges();
+
+    store = TestBed.inject(MockStore);
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  describe('when ioConnected === undefined', () => {
+    it('should not render the banner', () => {
+      expect(() => ngMocks.find('.offline-state-banner')).toThrow();
+    });
+  });
+
+  describe('when ioConnected === false', () => {
+    beforeEach(fakeAsync(() => {
+      ioConnected.setResult(false);
+      store.refreshState();
+      tick(1100);
+      fixture.detectChanges();
+    }));
+
+    it('should render the banner', () => {
+      expect(ngMocks.find('.offline-state-banner')).toBeTruthy();
+    });
+  });
+
+  describe('when ioConnected === true', () => {
+    beforeEach(() => {
+      ioConnected.setResult(true);
+      store.refreshState();
+      fixture.detectChanges();
+    });
+
+    it('should not render the banner', () => {
+      expect(() => ngMocks.find('.offline-state-banner')).toThrow();
+    });
+  });
+});
---
+++ src/app/core/offline-state-banner/offline-state-banner.component.ts
@@ -0,0 +1,29 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { ioConnected } from '@app/io/io.selectors';
+import { delay, switchMap } from 'rxjs/operators';
+import { of } from 'rxjs';
+
+@Component({
+  selector: 'app-offline-state-banner',
+  templateUrl: './offline-state-banner.component.html',
+  styleUrls: ['./offline-state-banner.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class OfflineStateBannerComponent {
+
+  ioConnected = this.store.select(ioConnected).pipe(
+    switchMap(connected => {
+      if (connected) {
+        return of(connected);
+      } else {
+        return of(connected).pipe(delay(1000));
+      }
+    }),
+  );
+
+  constructor(
+    private store: Store,
+  ) { }
+
+}
