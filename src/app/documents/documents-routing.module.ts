c785c2ca9bd6876bc1b20ead8da32d56534b8b31
Michał Garapich <michal@garapich.pl>
2021-03-09 16:42:15 +0100

    add Documents enum

--- src/app/documents/documents-routing.module.ts
+++ src/app/documents/documents-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RulesComponent } from './rules/rules.component';
 import { DocumentResolver } from './document.resolver';
+import { Documents } from './documents';
 
 const routes: Routes = [
   {
     path: 'rules',
     component: RulesComponent,
     data: {
-      documentName: 'rules',
+      documentName: Documents.rules,
       animation: 'RulesPage',
       title: 'rules',
     },
