2c2c1152f5cd0b08701b4d605c05c5cb8097f2f3
Michał Garapich <michal@garapich.pl>
2021-03-15 00:00:37 +0100

    feat(*): tf2pickup.de branding (#795)

---
+++ src/environments/tf2pickup.de.ts
@@ -0,0 +1,21 @@
+import { version } from 'package.json';
+import { entryQueue6v6 } from './entry-queue-6v6';
+
+const discordInvitation = 'https://discord.gg/bfVxT2STBe';
+
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.tf2pickup.de',
+  wsUrl: 'https://api.tf2pickup.de',
+  version,
+  headerLinks: [
+    { name: 'discord', icon: '/assets/ui_icon_discord.png', target: discordInvitation, tooltip: 'Join us on discord!' },
+  ],
+  footerLinks: [
+    { name: 'github', target: 'https://github.com/tf2pickup-pl' },
+    { name: 'discord', target: discordInvitation },
+    { name: 'changelog', target: 'https://github.com/tf2pickup-pl/client/blob/master/CHANGELOG.md' },
+  ],
+  titleSuffix: 'tf2pickup.de • Deutsche Team Fortress 2 Pick-up games',
+  entryQueue: entryQueue6v6,
+};
