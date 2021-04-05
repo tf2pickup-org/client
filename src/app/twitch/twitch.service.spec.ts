0b134a32d33048a21693239de991361657b6f5da
Michal Garapich <michal@garapich.pl>
2021-04-05 13:15:40 +0200

    fix(profile): get rid of warning when opening the popup

--- src/app/twitch/twitch.service.spec.ts
+++ src/app/twitch/twitch.service.spec.ts
@@ -3,19 +3,11 @@ import { TwitchService } from './twitch.service';
 import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
 import { API_URL } from '@app/api-url';
 import { provideMockStore } from '@ngrx/store/testing';
-import { AuthService } from '@app/auth/auth.service';
 import { WindowHelperService } from '@app/shared/window-helper.service';
-import { of } from 'rxjs';
 import { Socket } from '@app/io/socket';
 import EventEmitter from 'eventemitter3';
-
-class AuthServiceStub {
-  reauth() { return of('FAKE_AUTH_TOKEN'); }
-}
-
-class WindowHelperServiceStub {
-  openWindow(params: any) { }
-}
+import { TokenStoreService } from '@app/auth/token-store.service';
+import { MockProvider } from 'ng-mocks';
 
 describe('TwitchService', () => {
   let service: TwitchService;
@@ -30,9 +22,9 @@ describe('TwitchService', () => {
       providers: [
         { provide: API_URL, useValue: 'FAKE_URL' },
         provideMockStore(),
-        { provide: AuthService, useClass: AuthServiceStub },
-        { provide: WindowHelperService, useClass: WindowHelperServiceStub },
         { provide: Socket, useClass: EventEmitter },
+        MockProvider(TokenStoreService, { authToken: 'FAKE_AUTH_TOKEN' }),
+        MockProvider(WindowHelperService),
       ],
     });
 
@@ -57,9 +49,9 @@ describe('TwitchService', () => {
 
   describe('#login()', () => {
     it('should open a new window', () => {
-      const spy = spyOn(TestBed.inject(WindowHelperService), 'openWindow');
+      const windowHelperService = TestBed.inject(WindowHelperService) as jasmine.SpyObj<WindowHelperService>;
       service.login();
-      expect(spy).toHaveBeenCalledWith(jasmine.objectContaining({
+      expect(windowHelperService.openWindow).toHaveBeenCalledWith(jasmine.objectContaining({
         url: 'FAKE_URL/twitch/auth?token=FAKE_AUTH_TOKEN',
       }));
     });
