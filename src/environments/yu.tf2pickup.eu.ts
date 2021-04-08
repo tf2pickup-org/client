54610976bc3ebeb8a46c5f049c1a9cec4461c0b8
Vukasin Gostovic <vukasin@gostovic.de>
2021-04-08 18:33:33 +0200

    feat(yu.tf2pickup.pl): add yu.tf2pickup.eu branding (#837)

---
+++ src/environments/yu.tf2pickup.eu.ts
@@ -0,0 +1,21 @@
+import { version } from 'package.json';
+import { entryQueue6v6 } from './entry-queue-6v6';
+
+const discordInvitation = 'https://discord.gg/gvYMf2v';
+
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.yu.tf2pickup.eu',
+  wsUrl: 'https://api.yu.tf2pickup.eu',
+  version,
+  headerLinks: [
+    { name: 'discord', icon: '/assets/ui_icon_discord.png', target: discordInvitation, tooltip: 'Join us on discord!' },
+  ],
+  footerLinks: [
+    { name: 'github', target: 'https://github.com/makemake-kbo/client' },
+    { name: 'discord', target: discordInvitation },
+    { name: 'changelog', target: 'https://github.com/makemake-kbo/client/blob/master/CHANGELOG.md' },
+  ],
+  titleSuffix: 'yu.tf2pickup.eu • Ex-Yu Team Fortress 2 Pick-up games',
+  entryQueue: entryQueue6v6,
+};
