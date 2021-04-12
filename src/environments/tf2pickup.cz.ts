b4412945bc115154f7b69ffb1def4d6f0f9d9098
Michał Garapich <michal@garapich.pl>
2021-04-12 21:09:08 +0200

    fix(tf2pickup.cz): fix typo

--- src/environments/tf2pickup.cz.ts
+++ src/environments/tf2pickup.cz.ts
@@ -17,6 +17,7 @@ export const environment = {
     { name: 'discord', target: discordInvitation },
     { name: 'changelog', target: changelog },
   ],
-  titleSuffix: 'tf2pickup.cz • Czech & Slovakian Team Fortress 2 Pickup-up games',
+  titleSuffix:
+    'tf2pickup.cz • Czech & Slovakian Team Fortress 2 Pick-up games',
   entryQueue: entryQueue6v6,
 };
