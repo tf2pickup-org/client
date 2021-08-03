33556c3d1d85df2e04c51bf1df1c5968c7a4fe80
Michał Garapich <michal@garapich.pl>
2021-08-03 02:53:31 +0200

    fix(players): handle lack of player's roles

--- src/app/players/is-admin.pipe.ts
+++ src/app/players/is-admin.pipe.ts
@@ -6,6 +6,6 @@ import { Player } from './models/player';
 })
 export class IsAdminPipe implements PipeTransform {
   transform(player: Player): boolean {
-    return player?.roles.includes('admin') ?? false;
+    return player?.roles?.includes('admin') ?? false;
   }
 }
