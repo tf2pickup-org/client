3890b5b510299a1ebc52d658fd211b81c22b487a
Michał Garapich <michal@garapich.pl>
2021-08-09 15:20:30 +0200

    feat(tf2pickup.dk): add tf2pickup.dk branding

---
+++ src/environments/tf2pickup.dk.ts
@@ -0,0 +1,23 @@
+import { defaultEnvironment } from './default-environment';
+import { defaultFooterLinks } from './default-footer-links';
+
+const discordInvitation = 'https://discord.gg/RznhDSPrsa';
+
+export const environment = {
+  ...defaultEnvironment,
+  apiUrl: 'https://api.tf2pickup.dk',
+  wsUrl: 'https://api.tf2pickup.dk',
+  headerLinks: [
+    {
+      name: 'discord',
+      icon: '/assets/ui_icon_discord.png',
+      target: discordInvitation,
+      tooltip: 'Join us on discord!',
+    },
+  ],
+  footerLinks: [
+    ...defaultFooterLinks,
+    { name: 'discord', target: discordInvitation },
+  ],
+  titleSuffix: 'tf2pickup.dk • Dansk TF2 Pickup',
+};
