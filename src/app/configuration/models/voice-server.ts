75725188b33918ff561b2eb54f1cd32fa950ecdd
Michał Garapich <michal@garapich.pl>
2021-08-31 16:08:32 +0200

    feat(admin): static link voice server option

--- src/app/configuration/models/voice-server.ts
+++ src/app/configuration/models/voice-server.ts
@@ -3,11 +3,13 @@ import { MumbleOptions } from './mumble-options';
 
 export enum SelectedVoiceServer {
   none = 'none',
+  staticLink = 'static link',
   mumble = 'mumble',
 }
 
 export interface VoiceServer {
   key: ConfigurationEntryKey.voiceServer;
   type: SelectedVoiceServer;
+  staticLink?: string;
   mumble?: MumbleOptions;
 }
