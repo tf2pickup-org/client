ce68ee9581aed56b493151eb9fd22f38c04282e8
Michał Garapich <michal@garapich.pl>
2021-09-03 13:07:37 +0200

    fix(tf2pickup.ru): add discord invitation (#1200)

--- src/environments/tf2pickup.ru.ts
+++ src/environments/tf2pickup.ru.ts
@@ -1,11 +1,23 @@
 import { defaultEnvironment } from './default-environment';
 import { defaultFooterLinks } from './default-footer-links';
 
+const discordInvitation = 'https://discord.gg/jUATV48cYQ';
+
 export const environment = {
   ...defaultEnvironment,
   apiUrl: 'https://api.tf2pickup.ru',
   wsUrl: 'https://api.tf2pickup.ru',
-  headerLinks: [],
-  footerLinks: [...defaultFooterLinks],
-  titleSuffix: 'tf2pickup.ru • тф2пикап по-русски?',
+  headerLinks: [
+    {
+      name: 'discord',
+      icon: '/assets/ui_icon_discord.png',
+      target: discordInvitation,
+      tooltip: 'Join us on discord!',
+    },
+  ],
+  footerLinks: [
+    ...defaultFooterLinks,
+    { name: 'discord', target: discordInvitation },
+  ],
+  titleSuffix: 'tf2pickup.ru • Русские пикапы',
 };
