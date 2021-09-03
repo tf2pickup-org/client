89510ab5cf6b1da1bdd862ad3b27672d77a69a62
Michał Garapich <michal@garapich.pl>
2021-09-03 12:53:18 +0200

    feat(tf2pickup.co.il): add tf2pickup.co.il branding

---
+++ src/environments/tf2pickup.co.il.ts
@@ -0,0 +1,23 @@
+import { defaultEnvironment } from './default-environment';
+import { defaultFooterLinks } from './default-footer-links';
+
+const discordInvitation = 'https://discord.gg/jwSrfAKS';
+
+export const environment = {
+  ...defaultEnvironment,
+  apiUrl: 'https://api.tf2pickup.co.il',
+  wsUrl: 'https://api.tf2pickup.co.il',
+  headerLinks: [
+    {
+      name: 'discord',
+      icon: '/assets/ui_icon_discord.png',
+      target: discordInvitation,
+      tooltip: 'Join us on discord!',
+    },
+  ],
+  footerLinks: [
+    ...defaultFooterLinks,
+    { name: 'discord', target: discordInvitation },
+  ],
+  titleSuffix: 'tf2pickup.co.il • Israeli Team Fortress 2 Pick-up games',
+};
