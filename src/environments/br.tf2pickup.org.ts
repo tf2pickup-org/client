c558a476dbc049ebfb0029107185be62802b804e
Michał Garapich <michal@garapich.pl>
2021-10-31 23:47:47 +0100

    feat(br.tf2pickup.org): add br.tf2pickup.org branding (#1316)

---
+++ src/environments/br.tf2pickup.org.ts
@@ -0,0 +1,25 @@
+import { defaultEnvironment } from './default-environment';
+import { defaultFooterLinks } from './default-footer-links';
+import { defaultHeaderLinks } from './default-header-links';
+
+const discordInvitation = 'https://discord.gg/ZPNgmzzMvh';
+
+export const environment = {
+  ...defaultEnvironment,
+  apiUrl: 'https://api.br.tf2pickup.org',
+  wsUrl: 'https://api.br.tf2pickup.org',
+  headerLinks: [
+    {
+      name: 'discord',
+      icon: '/assets/ui_icon_discord.png',
+      target: discordInvitation,
+      tooltip: 'Join us on discord!',
+    },
+    ...defaultHeaderLinks,
+  ],
+  footerLinks: [
+    ...defaultFooterLinks,
+    { name: 'discord', target: discordInvitation },
+  ],
+  titleSuffix: 'br.tf2pickup.org • Mixes Brasileiros de TF2',
+};
