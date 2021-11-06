177c6c63364f5a3316eff4ff59e128ae493aac17
Michał Garapich <michal@garapich.pl>
2021-11-06 01:34:56 +0100

    fix(game-servers): sort game servers by priority

--- src/app/game-servers/game-servers.adapter.ts
+++ src/app/game-servers/game-servers.adapter.ts
@@ -2,4 +2,6 @@ import { EntityAdapter, createEntityAdapter } from '@ngrx/entity';
 import { GameServer } from './models/game-server';
 
 export const adapter: EntityAdapter<GameServer> =
-  createEntityAdapter<GameServer>();
+  createEntityAdapter<GameServer>({
+    sortComparer: (a: GameServer, b: GameServer) => b.priority - a.priority,
+  });
--- src/app/game-servers/models/game-server.ts
+++ src/app/game-servers/models/game-server.ts
@@ -4,4 +4,5 @@ export interface GameServer {
   address: string;
   port: string;
   isOnline: boolean;
+  priority: number;
 }
