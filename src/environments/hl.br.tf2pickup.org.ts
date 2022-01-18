e4fa92a486166059e6cdbebf9b595af2101cbf9f
Michał Garapich <michal@garapich.pl>
2022-01-18 01:10:41 +0100

    feat(hl.br.tf2pickup.org): add hl.br.tf2pickup.org branding

---
+++ src/environments/hl.br.tf2pickup.org.ts
@@ -0,0 +1,27 @@
+import { defaultEnvironment } from './default-environment';
+import { defaultFooterLinks } from './default-footer-links';
+import { defaultHeaderLinks } from './default-header-links';
+import { entryQueue9v9 } from './entry-queues/entry-queue-9v9';
+
+const discordInvitation = 'https://discord.gg/eJfrSPVYZy';
+
+export const environment = {
+  ...defaultEnvironment,
+  apiUrl: 'https://api.br.tf2pickup.org',
+  wsUrl: 'https://api.br.tf2pickup.org',
+  headerLinks: [
+    {
+      name: 'discord',
+      icon: '/assets/ui_icon_discord.png',
+      target: discordInvitation,
+      tooltip: 'Join us on discord!',
+    },
+    ...defaultHeaderLinks,
+  ],
+  footerLinks: [
+    ...defaultFooterLinks,
+    { name: 'discord', target: discordInvitation },
+  ],
+  titleSuffix: 'hl.br.tf2pickup.org • Mixes Brasileiros de TF2',
+  entryQueue: entryQueue9v9,
+};
