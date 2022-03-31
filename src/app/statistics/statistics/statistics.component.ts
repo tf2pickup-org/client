58022e402247c8fd60a4650da3f31160f2c747c9
Michał Garapich <michal@garapich.pl>
2022-03-31 23:02:33 +0200

    use echarts

--- src/app/statistics/statistics/statistics.component.ts
+++ src/app/statistics/statistics/statistics.component.ts
@@ -2,6 +2,8 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { StatisticsService } from '@app/statistics/statistics.service';
 import { concat, dropRight, takeRight } from 'lodash';
 import { map } from 'rxjs/operators';
+import { EChartsOption } from 'echarts';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-statistics',
@@ -10,17 +12,37 @@ import { map } from 'rxjs/operators';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class StatisticsComponent {
-  playedMapCount = this.statisticsService.fetchPlayedMapsCount().pipe(
-    map(data => data.map(d => ({ label: d.mapName, value: d.count }))),
-    map(data => {
-      const lastTenMaps = takeRight(data, Math.max(0, data.length - 8));
-      const other = {
-        label: 'other maps',
-        value: lastTenMaps.reduce((sum, d) => sum + d.value, 0),
-      };
-      return concat(dropRight(data, Math.max(0, data.length - 8)), other);
-    }),
-  );
+  playedMapCount: Observable<EChartsOption> = this.statisticsService
+    .fetchPlayedMapsCount()
+    .pipe(
+      map(data => data.map(d => ({ name: d.mapName, value: d.count }))),
+      map(data => {
+        const lastTenMaps = takeRight(data, Math.max(0, data.length - 8));
+        const other = {
+          name: 'other maps',
+          value: lastTenMaps.reduce((sum, d) => sum + d.value, 0),
+        };
+        return concat(dropRight(data, Math.max(0, data.length - 8)), other);
+      }),
+      map(data => ({
+        title: {
+          text: 'Most played maps',
+          x: 'center',
+        },
+        calculable: true,
+        series: [
+          {
+            name: 'area',
+            type: 'pie',
+            startAngle: 90,
+            data,
+          },
+        ],
+        label: {
+          formatter: '{b}: {c}',
+        },
+      })),
+    );
 
   constructor(private statisticsService: StatisticsService) {}
 }
