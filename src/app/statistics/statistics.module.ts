c6f129eed531fd4abafa14145060a69f3d1c5349
Michał Garapich <michal@garapich.pl>
2022-03-31 22:31:18 +0200

    dev

--- src/app/statistics/statistics.module.ts
+++ src/app/statistics/statistics.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StatisticsComponent } from './statistics/statistics.component';
 import { StatisticsRoutingModule } from './statistics-routing.module';
+import { PieChartComponent } from './pie-chart/pie-chart.component';
 
 @NgModule({
-  declarations: [StatisticsComponent],
+  declarations: [StatisticsComponent, PieChartComponent],
   imports: [CommonModule, StatisticsRoutingModule],
 })
 export class StatisticsModule {}
--- src/app/statistics/statistics/statistics.component.ts
+++ src/app/statistics/statistics/statistics.component.ts
@@ -1,5 +1,7 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { StatisticsService } from '@app/statistics/statistics.service';
+import { concat, dropRight, takeRight } from 'lodash';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-statistics',
@@ -8,7 +10,17 @@ import { StatisticsService } from '@app/statistics/statistics.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class StatisticsComponent {
-  playedMapCount = this.statisticsService.fetchPlayedMapsCount();
+  playedMapCount = this.statisticsService.fetchPlayedMapsCount().pipe(
+    map(data => data.map(d => ({ label: d.mapName, value: d.count }))),
+    map(data => {
+      const lastTenMaps = takeRight(data, Math.max(0, data.length - 8));
+      const other = {
+        label: 'other maps',
+        value: lastTenMaps.reduce((sum, d) => sum + d.value, 0),
+      };
+      return concat(dropRight(data, Math.max(0, data.length - 8)), other);
+    }),
+  );
 
   constructor(private statisticsService: StatisticsService) {}
 }
