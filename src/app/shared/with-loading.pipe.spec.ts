1a610c600bd895eadd19bcae87a1b10252bd37ff
Michał Garapich <michal@garapich.pl>
2022-10-26 15:47:32 +0200

    more tests

--- src/app/shared/with-loading.pipe.spec.ts
+++ src/app/shared/with-loading.pipe.spec.ts
@@ -1,8 +1,44 @@
+import { Subject } from 'rxjs';
 import { WithLoadingPipe } from './with-loading.pipe';
 
 describe('WithLoadingPipe', () => {
+  let pipe: WithLoadingPipe;
+
+  beforeEach(() => {
+    pipe = new WithLoadingPipe();
+  });
+
   it('create an instance', () => {
-    const pipe = new WithLoadingPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should set loading: true initially', () => {
+    const input = new Subject<number>();
+    let output = null;
+    pipe.transform(input).subscribe(v => (output = v));
+    expect(output).toEqual({ loading: true });
+    input.complete();
+  });
+
+  describe('when the value is loaded', () => {
+    it('should set the given value', () => {
+      const input = new Subject<number>();
+      let output = null;
+      pipe.transform(input).subscribe(v => (output = v));
+      input.next(42);
+      expect(output).toEqual({ loading: false, value: 42 });
+      input.complete();
+    });
+  });
+
+  describe('when an error occurs', () => {
+    it('should set the error', () => {
+      const input = new Subject<number>();
+      let output = null;
+      pipe.transform(input).subscribe(v => (output = v));
+      input.error('FAKE_ERROR');
+      expect(output).toEqual({ loading: false, error: 'FAKE_ERROR' });
+      input.complete();
+    });
   });
 });
