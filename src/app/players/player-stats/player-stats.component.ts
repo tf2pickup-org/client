6b2a62ed3a41a88dc6436e55183f86c55e550391
Michał Garapich <michal@garapich.pl>
2022-12-12 14:04:44 +0100

    fix(players): order game classes in player stats (#2077)

--- src/app/players/player-stats/player-stats.component.ts
+++ src/app/players/player-stats/player-stats.component.ts
@@ -1,4 +1,7 @@
+import { KeyValue } from '@angular/common';
 import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+import { Tf2ClassName } from '@app/shared/models/tf2-class-name';
+import { tf2ClassOrder } from '@app/shared/tf2-class-order';
 import { PlayerStats } from '../models/player-stats';
 
 @Component({
@@ -10,4 +13,10 @@ import { PlayerStats } from '../models/player-stats';
 export class PlayerStatsComponent {
   @Input()
   playerStats: PlayerStats;
+
+  // skipcq: JS-0105
+  orderGameClasses = (
+    a: KeyValue<Tf2ClassName, number>,
+    b: KeyValue<Tf2ClassName, number>,
+  ): number => tf2ClassOrder[b.key] - tf2ClassOrder[a.key];
 }
