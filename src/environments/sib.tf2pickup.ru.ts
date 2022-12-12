612b266bb24a4394b42c87c38208e44ce34e637c
Michał Garapich <michal@garapich.pl>
2022-12-12 15:54:05 +0100

    feat(sib.tf2pickup.ru): add sib.tf2pickup.ru branding (#2080)

---
+++ src/environments/sib.tf2pickup.ru.ts
@@ -0,0 +1,25 @@
+import { defaultEnvironment } from './default-environment';
+import { defaultFooterLinks } from './default-footer-links';
+import { defaultHeaderLinks } from './default-header-links';
+
+const discordInvitation = 'https://discord.gg/jUATV48cYQ';
+
+export const environment = {
+  ...defaultEnvironment,
+  apiUrl: 'https://api.sib.tf2pickup.ru',
+  wsUrl: 'https://api.sib.tf2pickup.ru',
+  headerLinks: [
+    {
+      name: 'discord',
+      icon: '/assets/ui_icon_discord.png',
+      target: discordInvitation,
+      tooltip: 'Join us on discord!',
+    },
+    ...defaultHeaderLinks,
+  ],
+  footerLinks: [
+    ...defaultFooterLinks,
+    { name: 'discord', target: discordInvitation },
+  ],
+  titleSuffix: 'sib.tf2pickup.ru • Сибирские пикапы',
+};
