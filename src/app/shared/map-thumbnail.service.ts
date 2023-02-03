a716d69bd8ef043362d5fec6317bc75e9a28e1fd
Michał Garapich <michal@garapich.pl>
2023-02-04 00:44:18 +0100

    fix(queue, games): add MapThumbnailComponent

--- src/app/shared/map-thumbnail.service.ts
+++ src/app/shared/map-thumbnail.service.ts
@@ -1,8 +1,5 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { environment } from '@environment';
-import { catchError, map, Observable, of } from 'rxjs';
 
 interface MapThumbnailUrlParams {
   width: number;
@@ -13,11 +10,6 @@ interface MapThumbnailUrlParams {
   providedIn: 'root',
 })
 export class MapThumbnailService {
-  constructor(
-    private readonly httpClient: HttpClient,
-    private readonly sanitizer: DomSanitizer,
-  ) {}
-
   // skipcq: JS-0105
   getMapThumbnailUrl(
     map: string,
@@ -25,23 +17,5 @@ export class MapThumbnailService {
   ) {
     const mapName = map.match(/^([a-z]+_[a-zA-Z0-9]+)/)?.[0] ?? 'unknown';
     return `${environment.mapThumbnailServiceUrl}/unsafe/${width}x${height}/${mapName}.jpg`;
-  }
-
-  getMapThumbnail(
-    _map: string,
-    params: MapThumbnailUrlParams = { width: 300, height: 169 },
-  ): Observable<SafeUrl> {
-    const url = this.getMapThumbnailUrl(_map, params);
-    return this.httpClient.get(url, { responseType: 'blob' }).pipe(
-      map(blob => URL.createObjectURL(blob)),
-      map(objectUrl => this.sanitizer.bypassSecurityTrustUrl(objectUrl)),
-      catchError((error: unknown) => {
-        if (error instanceof HttpErrorResponse && error.status === 404) {
-          return of('/assets/unknown-map.png');
-        } else {
-          throw error;
-        }
-      }),
-    );
   }
 }
---
+++ src/app/shared/map-thumbnail/map-thumbnail.component.ts
@@ -0,0 +1,40 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnChanges,
+} from '@angular/core';
+import { MapThumbnailService } from '../map-thumbnail.service';
+
+@Component({
+  selector: 'app-map-thumbnail',
+  templateUrl: './map-thumbnail.component.html',
+  styleUrls: ['./map-thumbnail.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MapThumbnailComponent implements OnChanges {
+  src?: string;
+  loaded = false;
+
+  @Input()
+  map: string;
+
+  @Input()
+  width = 300;
+
+  @Input()
+  height = 160;
+
+  constructor(private readonly mapThumbnailService: MapThumbnailService) {}
+
+  ngOnChanges() {
+    this.src = this.mapThumbnailService.getMapThumbnailUrl(this.map, {
+      width: this.width,
+      height: this.height,
+    });
+  }
+
+  handleError() {
+    this.src = '/assets/unknown-map.png';
+  }
+}
