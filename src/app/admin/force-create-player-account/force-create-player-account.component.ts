a14bca2d19b0b38890bf8b1616cc9e9a4d734f35
Michał Garapich <michal@garapich.pl>
2023-06-29 19:10:30 +0200

    fix(admin): fix ForceCreatePlayerAccount page (#2344)

--- src/app/admin/force-create-player-account/force-create-player-account.component.ts
+++ src/app/admin/force-create-player-account/force-create-player-account.component.ts
@@ -1,15 +1,7 @@
 import { Location } from '@angular/common';
-import {
-  Component,
-  ChangeDetectionStrategy,
-  ElementRef,
-  ViewChild,
-  AfterViewInit,
-  OnDestroy,
-} from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { PlayersService } from '@app/players/players.service';
-import { MDCTextField } from '@material/textfield';
 
 @Component({
   selector: 'app-force-create-player-account',
@@ -17,43 +9,21 @@ import { MDCTextField } from '@material/textfield';
   styleUrls: ['./force-create-player-account.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ForceCreatePlayerAccountComponent
-  implements AfterViewInit, OnDestroy
-{
+export class ForceCreatePlayerAccountComponent {
   form = this.formBuilder.group({
     name: ['', Validators.required],
     steamId: ['', [Validators.required, Validators.pattern(/^\d{17}$/)]],
   });
 
-  @ViewChild('name')
-  nameInput: ElementRef;
-
-  @ViewChild('steamId')
-  steamIdInput: ElementRef;
-
-  private fields: MDCTextField[];
-
   constructor(
-    private formBuilder: FormBuilder,
-    private playersService: PlayersService,
-    private location: Location,
+    private readonly formBuilder: FormBuilder,
+    private readonly playersService: PlayersService,
+    private readonly location: Location,
   ) {}
 
   save() {
     this.playersService
       .forceCreatePlayer(this.form.value)
       .subscribe(() => this.location.back());
-  }
-
-  ngAfterViewInit() {
-    this.fields = [this.nameInput, this.steamIdInput].map(
-      input => new MDCTextField(input.nativeElement),
-    );
-
-    setTimeout(() => this.nameInput.nativeElement.focus(), 0);
-  }
-
-  ngOnDestroy() {
-    this.fields.forEach(field => field.destroy());
   }
 }
